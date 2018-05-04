15b5717e5a251991ba1f6df9f494c38cc5eeb836
joaocabral6 <38953911+joaocabral6@users.noreply.github.com>
2018-05-03 22:17:41 -0300

    Add files via upload

---
+++ fila.h
@@ -0,0 +1,11 @@
+#ifndef FILA_H_INCLUDED
+#define FILA_H_INCLUDED
+
+aviao *inserir(aviao **inicio, aviao **fim, aviao *novo);
+aviao *remover(aviao **inicio);
+void *contar(aviao **inicio, aviao **fim);
+void *imprimir(aviao **inicio, aviao **fim);
+void *consultar_cabeça(aviao **inicio);
+void *quantidadeDePessoas(aviao **inicio, aviao **fim);
+
+#endif // FILA_H_INCLUDED
