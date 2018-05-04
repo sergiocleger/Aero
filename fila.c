4ac973934ddd47184ec3e29d2f0e64f175246659
joaocabral6 <38953911+joaocabral6@users.noreply.github.com>
2018-05-03 22:26:47 -0300

    Add files via upload

---
+++ fila.c
@@ -0,0 +1,26 @@
+#include "fila.h"
+#include "lista.h"
+
+aviao *inserir(aviao **inicio, aviao **fim, aviao *novo){
+
+}
+
+aviao *remover(aviao **inicio){
+
+}
+
+void *contar(aviao **inicio, aviao **fim){
+
+}
+
+void *imprimir(aviao **inicio, aviao **fim){
+
+}
+
+void *consultar_cabeca(aviao **inicio){
+
+}
+
+void *quantidadeDePessoas(aviao **inicio, aviao **fim){
+
+}
