470088f7d060faf61f041f4ac9ca38f55e79ad63
Ronison Melo <38872876+R0nison@users.noreply.github.com>
2018-05-03 23:07:33 -0400

    Add files via upload

---
+++ CRIANDO AVIAO E INSERINDO NA LISTA - RONISON.c
@@ -0,0 +1,18 @@
+#include "lista.h"
+aviao* criar_aviao(char codigo[7],int num,char cap[20]){
+	aviao *aux;
+	aux=(aviao*)malloc(sizeof(aviao));
+	strcpy(aux->codigo,codigo);
+	strcpy(aux->capitao,cap);
+	aux->numero_p=num;
+	aux->prox=NULL;
+}
+void insere(aviao **inicio,char codigo[7],int nump,char cap[20]){
+	aviao *aux=criar_aviao(codigo,nump,cap);
+	if((*inicio)==NULL){
+		(*inicio)=aux;
+	}else{
+		aux->prox=(*inicio);
+		(*inicio)=aux;
+	}
+}
