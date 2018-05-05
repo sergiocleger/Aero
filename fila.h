73fa0472632343d7219758cd79a92d8ddadef9a1
joaocabral6 <38953911+joaocabral6@users.noreply.github.com>
2018-05-05 14:33:35 -0400

    Update fila.h

--- fila.h
+++ fila.h
@@ -7,5 +7,6 @@ void *contar(aviao **inicio, aviao **fim);
 void *imprimir(aviao **inicio, aviao **fim);
 void *consultar_cabeca(aviao **inicio);
 void *quantidadeDePessoas(aviao **inicio, aviao **fim);
+void *imprime_dado_voo(aviao **inicio, aviao **fim); //HENRIQUE
 
 #endif // FILA_H_INCLUDED
