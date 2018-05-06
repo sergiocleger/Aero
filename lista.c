32d22b0889498552b515af72dd0e4919d0761721
cezarograde <38872753+cezarograde@users.noreply.github.com>
2018-05-06 16:53:22 -0400

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,8 +1,17 @@
 #include "lista.h"
 #include "fila.h"
 
-void insere(aviao **inicio,char codigo[7],int numero_p, char capitao[20]){
+void insere(aviao **lista,char codigo[7],int numero_p, char capitao[20]){
 
+    aviao *p, *inicio;
+				if((*lista)==NULL){
+						p=criar_aviao(codigo,capitao,numero_p);
+						(*lista)=inicio=p;
+			}else{
+						p=criar_aviao(codigo,capitao,numero_p);
+						(*lista)->prox=p;
+						inicio=p;
+				}
 }//CESAR
 
 aviao* criar_aviao(char codigo[7],int numero_p, char capitao[20]){
@@ -20,7 +29,7 @@ void check_lista(aviao **aviao, char codigo[7]){
         }
     }
     if(booleano == 0){
-        printf("CODIGO N√O ENCONTRADO!\n");
+        printf("CODIGO N√ÉO ENCONTRADO!\n");
     }
 }//JOAO C
 
