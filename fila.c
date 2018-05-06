360e068e0f6c94777354823760e80f0f0098a35e
Matheus Silva Coelho dos Santos <38893616+Matheuz18@users.noreply.github.com>
2018-05-05 22:08:16 -0300

    Update fila.c

--- fila.c
+++ fila.c
@@ -14,8 +14,20 @@ void *contar(aviao **inicio, aviao **fim){
 }
 
 void *imprimir(aviao **inicio, aviao **fim){
+    int i=1;
+    aviao *aux;
+    aux = *inicio;
 
-}
+    do{
+      printf("Aviao N%d\n",i);
+      printf("codigo - %s \n",(aux)->codigo);
+      printf("capitao - %s \n",(aux)->capitao);
+    //	printf("Destino - %s \n",(aux)->destino);
+      printf("numero de passageiros - %d \n\n\n",(aux)->numero_p);
+      (aux)=(aux)->prox;
+      i++;
+    }while(aux != NULL);
+}//matheus
 
 void *consultar_cabeca(aviao **inicio){
 
